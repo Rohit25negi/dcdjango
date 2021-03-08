5eee64a0e8c898121ab51f3e5d9bdde1cd5ef432
EC2 Default User <ec2-user@ip-172-31-47-179.ap-south-1.compute.internal>
2021-03-08 02:09:39 +0000

    First commit

---
+++ test_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class TestAPI(APIView):
+    
+    def get(self, request, format=None):
+        return Response({"message":" hello world!!!"})
